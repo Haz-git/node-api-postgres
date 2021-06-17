46c34fbee27870fa9927de46ce84de3c97940398
Haz-git <zhouharry1@gmail.com>
2021-06-17 09:01:09 -0700

    Attached queries

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//our query exports:
+const db = require('./queries');
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
+//default homepage query
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API '});
 });
+
+//New test queries
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
